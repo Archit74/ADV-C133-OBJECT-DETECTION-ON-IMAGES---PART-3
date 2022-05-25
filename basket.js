a1ffb7a332fee6757f23b4306a68fc8960ec6483
Archit74 <81956741+Archit74@users.noreply.github.com>
2022-05-25 21:10:04 +0530

    Add files via upload

---
+++ basket.js
@@ -0,0 +1,4 @@
+function basket_back()
+{
+    window.location="index.html";
+}
---
+++ main.js
@@ -0,0 +1,24 @@
+function bed()
+{
+    window.location="bedroom.html";
+}
+
+function ac()
+{
+    window.location="AC and TV.html";
+}
+
+function desk()
+{
+    window.location="desk.html";
+}
+
+function bottle()
+{
+    window.location="bottle.html";
+}
+
+function basket()
+{
+    window.location="basket.html";
+}
